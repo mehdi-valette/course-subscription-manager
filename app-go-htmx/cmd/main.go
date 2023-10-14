37e61f85df54acaa279943015ec07a8c6b552477
Mehdi Valette <mehdi.valette@gmail.com>
2023-10-14 15:25:44 +0200

    style(student): improve the UI

--- app-go-htmx/cmd/main.go
+++ app-go-htmx/cmd/main.go
@@ -3,9 +3,31 @@ package main
 import (
 	"app-htmx/routing"
 	"net/http"
+	"os/exec"
+	"runtime"
 )
 
 func main() {
 	http.HandleFunc("/", routing.FrontController)
 	http.ListenAndServe(":5000", nil)
+	open("http://localhost:5000")
+}
+
+// https://stackoverflow.com/questions/39320371/how-start-web-server-to-open-page-in-browser-in-golang
+// open opens the specified URL in the default browser of the user.
+func open(url string) error {
+	var cmd string
+	var args []string
+
+	switch runtime.GOOS {
+	case "windows":
+		cmd = "cmd"
+		args = []string{"/c", "start"}
+	case "darwin":
+		cmd = "open"
+	default: // "linux", "freebsd", "openbsd", "netbsd"
+		cmd = "xdg-open"
+	}
+	args = append(args, url)
+	return exec.Command(cmd, args...).Start()
 }
